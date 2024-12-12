a46320a5d530cad4b9fe8cb47c74f31db5f6b9a0
marc-antoine mercier <marcm1542@gmail.com>
2024-12-12 16:40:21 -0500

    remove junitTest

--- src/test/java/marc/AppTest.java
+++ src/test/java/marc/AppTest.java
@@ -1,21 +1,11 @@
 package marc;
 
-import marc.actionBd.AfficheSelectRequest;
-import marc.actionBd.SelectRequest;
-import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest
 {
-    @Test
-    void request() {
-        try {
-            new AfficheSelectRequest(new SelectRequest(null));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
+
 
 }
